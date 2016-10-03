61a03a89cdc688aae7c09d321e9814539a647b71
Nana Abankwa <nana.abankwa@gmail.com>
2016-10-03 12:26:38 +0100

    Added functions for moving and drawing

--- bounce.cc
+++ bounce.cc
@@ -2,29 +2,52 @@
 
 #include <iostream>
 
+char particleSymbol = 'x';
+int minColumn = 0;
+double particlePosition = minColumn;
+double particleSpeed = 6.3;
+int maxColumn = 80;
+
+void draw() 
+{
+	for (int i = 0; i < particlePosition; i++) 
+		{
+      			std::cout << " ";
+    		}
+    	std::cout << particleSymbol << std::endl;
+}
+
+void move()
+{
+	particlePosition += particleSpeed;
+	if (particlePosition >= maxColumn) 
+	{
+      		particlePosition = maxColumn;
+      		particleSpeed = -particleSpeed;
+    	} 
+	else if (particlePosition < minColumn) 
+	{
+		particlePosition = minColumn;
+		particleSpeed = -particleSpeed;
+	}
+}
+
 int main() {
 
-  char particleSymbol = 'x';
-  int minColumn = 0;
-  double particlePosition = minColumn;
-  double particleSpeed = 6.3;
-  int maxColumn = 80;
   int timeStep = 0;
   int stopTime = 60;
 
   while (timeStep < stopTime) {
+   
+    draw();
+
+    move();
+    timeStep++;
+  
+	}
+
     for (int i = 0; i < particlePosition; i++) {
       std::cout << " ";
     }
     std::cout << particleSymbol << std::endl;
-    particlePosition += particleSpeed;
-    if (particlePosition >= maxColumn) {
-      particlePosition = maxColumn;
-      particleSpeed = -particleSpeed;
-    } else if (particlePosition < minColumn) {
-      particlePosition = minColumn;
-      particleSpeed = -particleSpeed;
-    }    
-    timeStep++;
-  }
 }
