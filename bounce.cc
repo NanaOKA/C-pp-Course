dad58345d56a390d38cf9416402d628af2f11e63
Nana Abankwa <nana.abankwa@gmail.com>
2016-10-04 15:43:34 +0100

    Make move, draw, and initialize functions inside particle struct

--- bounce.cc
+++ bounce.cc
@@ -12,17 +12,45 @@ const int screenSize = maxColumn+1;
 
 struct Particle
 {
+
+	//void initialize(Particle * const, char, double, double);
+	//void draw(Particle const * const, char * const);
+	//void move(Particle * const);
+
 	char particleSymbol;
 	double particlePosition;
 	double particleSpeed;
+
+	void initialize(Particle * p, char particleSymbol, double particlePosition, double particleSpeed)
+	{
+		p->particleSymbol = particleSymbol;
+		p->particlePosition = particlePosition;
+		p->particleSpeed = particleSpeed;
+	}
+
+	void draw(Particle const * const p, char * const screen) 
+	{
+		screen[static_cast<int>(p->particlePosition)] = p->particleSymbol;
+	}
+
+	void move(Particle * const p)
+	{
+		p->particlePosition += p->particleSpeed;
+		if (p->particlePosition >= maxColumn) 
+		{
+	  		p->particlePosition = maxColumn;
+	    	p->particleSpeed = -p->particleSpeed;
+		} 
+		else if (p->particlePosition < minColumn) 
+		{
+			p->particlePosition = minColumn;
+			p->particleSpeed = -p->particleSpeed;
+		}
+	}
 };
 
-// char screen[screenSize];
 char* const screen = new char[screenSize];
 
-void initialize(Particle * const, char, double, double);
-void draw(Particle const * const, char * const);
-void move(Particle * const);
 void clear_screen(char * const);
 void print_screen(char * const);
 
@@ -30,20 +58,9 @@ int main()
 {
   Particle p[3];
 
-  initialize(&p[0], '*', minColumn, 1);
-  initialize(&p[1], '*', minColumn, 3.5);
-  initialize(&p[2], '*', minColumn, 5.9);
-  // char particleSymbol[] = {'*','+','^','o','*'};
-  // double particlePosition[] = {minColumn,minColumn+3,minColumn+5,minColumn+7,minColumn+11};
-  // double particleSpeed[] = {1,3,5,7,11};
-  
-  // const char particleSymbol = 'x';
-  // double particlePosition = minColumn;
-  // double particleSpeed = 6.3;	
-
-  // const char particleSymbol2 = '+';
-  // double particlePosition2 = minColumn;
-  // double particleSpeed2 = 8.3;
+  p[0].initialize(&p[0], '*', minColumn, 1);
+  p[1].initialize(&p[1], '*', minColumn, 3.5);
+  p[2].initialize(&p[2], '*', minColumn, 5.9);
 
   int timeStep = 0;
   const int stopTime = 100;
@@ -53,20 +70,10 @@ int main()
    	clear_screen(screen);
    	for (int i = 0 ; i < 3; i++)
    	{
-   		draw(&p[i], screen);
-   		move(&p[i]);
-
-   		// draw(*(particlePosition+i),*(particleSymbol+i),screen);
-   		// move(*(particlePosition+i),*(particleSpeed+i));
-
-   		// draw(particlePosition[i],particleSymbol[i],screen);
-   		// move(particlePosition[i],particleSpeed[i]);
+   		p[i].draw(&p[i], screen);
+   		p[i].move(&p[i]);
    	}
-    // draw(particlePosition,particleSymbol);
-    // draw(particlePosition2,particleSymbol2);
 
-    // move(particlePosition,particleSpeed);
-    // move(particlePosition2,particleSpeed2);
     print_screen(screen);
 
     timeStep++;
@@ -74,37 +81,6 @@ int main()
 	delete [] screen;
 }
 
-void initialize(Particle * p, char particleSymbol, double particlePosition, double particleSpeed)
-{
-	p->particleSymbol = particleSymbol;
-	p->particlePosition = particlePosition;
-	p->particleSpeed = particleSpeed;
-}
-void draw(Particle const * const p, char * const screen) 
-{
-	//screen[(int)particlePosition] = particleSymbol
-	screen[static_cast<int>(p->particlePosition)] = p->particleSymbol;
-	// for (int i = 0; i < particlePosition; i++) 
-	// 	{
- //      		std::cout << " ";
- //  	}
- //    std::cout << particleSymbol << std::endl;
-}
-
-void move(Particle * const p)
-{
-	p->particlePosition += p->particleSpeed;
-	if (p->particlePosition >= maxColumn) 
-	{
-  	p->particlePosition = maxColumn;
-    p->particleSpeed = -p->particleSpeed;
-	} 
-	else if (p->particlePosition < minColumn) 
-	{
-		p->particlePosition = minColumn;
-		p->particleSpeed = -p->particleSpeed;
-	}
-}
 
 void clear_screen(char * const screen)
 {
